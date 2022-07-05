ca09e40fcef119a5674572ab90dc84d440ec2231
lilianaaaaa <lilianaaa804@gmail.com>
2022-07-05 18:32:16 +0900

    두번째 테스트

--- src/main/java/PasswordStrengthMeter.java
+++ src/main/java/PasswordStrengthMeter.java
@@ -1,5 +1,10 @@
 public class PasswordStrengthMeter {
     public PasswordStrength meter(String s){
+        //return PasswordStrength.STRONG;
+        //return PasswordStrength.NORMAL;//두번째 테스트만 통과
+        if(s.length() < 8){
+            return PasswordStrength.NORMAL;
+        }
         return PasswordStrength.STRONG;
     }
 }
