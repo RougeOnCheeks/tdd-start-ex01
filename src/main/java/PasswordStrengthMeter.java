9915bfc5fc0d79405bed271fa2baaff837a658b1
lilianaaaaa <lilianaaa804@gmail.com>
2022-07-05 18:41:30 +0900

    세번째 테스트

--- src/main/java/PasswordStrengthMeter.java
+++ src/main/java/PasswordStrengthMeter.java
@@ -5,6 +5,28 @@ public class PasswordStrengthMeter {
         if(s.length() < 8){
             return PasswordStrength.NORMAL;
         }
+//        boolean containsNum = false;
+//        //각 문자를 비교해서 0~9 사이의 값을 갖는 문자가 있으면 true로 바꾸고 break, 없으면 NORMAL 리턴
+//        for(char ch: s.toCharArray()){
+//            if (ch >= '0' && ch <= '9') {
+//                containsNum = true;
+//                break;
+//            }
+//        }
+
+        boolean containsNum = meetsContainingNumberCritera(s);
+
+        if(!containsNum) return PasswordStrength.NORMAL;
+
         return PasswordStrength.STRONG;
+    }
+
+    private boolean meetsContainingNumberCritera(String s){
+        for(char ch: s.toCharArray()){
+            if (ch >= '0' && ch <= '9') {
+                return true;
+            }
+        }
+        return false;
     }
 }
