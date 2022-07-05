41b09809b60b96bbd624cbc9387729894964393b
lilianaaaaa <lilianaaa804@gmail.com>
2022-07-05 18:12:49 +0900

    initial

---
+++ src/main/java/Calculator.java
@@ -0,0 +1,6 @@
+public class Calculator {
+    public static int plus(int a1, int a2){
+        //return 3; 1번
+        return a1 + a2; //2번
+    }
+}
---
+++ src/test/java/CalculatorTest.java
@@ -0,0 +1,12 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class CalculatorTest {
+    @Test
+    void plus(){
+        int sum = Calculator.plus(1, 2);
+        assertEquals(3, sum); //1번
+        assertEquals(5, Calculator.plus(4,1));//2번
+    }
+}
