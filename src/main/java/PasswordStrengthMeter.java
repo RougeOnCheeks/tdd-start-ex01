f2463f4be0a2629bfc0b500436aa41d89b2097c5
lilianaaaaa <lilianaaa804@gmail.com>
2022-07-06 11:06:00 +0900

    일곱번째 테스트

--- src/main/java/PasswordStrengthMeter.java
+++ src/main/java/PasswordStrengthMeter.java
@@ -9,6 +9,9 @@ public class PasswordStrengthMeter {
         if(lengthEnough && !containsNum && !containsUpp){
             return PasswordStrength.WEAK;
         }
+        if(!lengthEnough && containsNum && !containsUpp){
+            return PasswordStrength.WEAK;
+        }
 
         if(!lengthEnough) return PasswordStrength.NORMAL;
         if(!containsNum) return PasswordStrength.NORMAL;
