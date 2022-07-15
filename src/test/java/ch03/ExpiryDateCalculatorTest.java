07b97ae0e2264cd8e2065b6e9fcceda86f990dba
lilianaaaaa <lilianaaa804@gmail.com>
2022-07-15 14:08:47 +0900

    리팩토링

--- src/test/java/ch03/ExpiryDateCalculatorTest.java
+++ src/test/java/ch03/ExpiryDateCalculatorTest.java
@@ -10,22 +10,13 @@ public class ExpiryDateCalculatorTest {
 
     @Test
     void 만원_납부하면_한달_뒤가_만료일이_됨(){
-        LocalDate billingDate = LocalDate.of(2022,7,1);
-        int payAmount = 10_000;
+        assertExpiryDate(LocalDate.of(2022,7,1), 10_000, LocalDate.of(2022,8,1));
+        assertExpiryDate(LocalDate.of(2022,7,5), 10_000, LocalDate.of(2022,8,5));
+    }
 
+    private void assertExpiryDate(LocalDate billingDate, int payAmount, LocalDate expectedExpiryDate){
         ExpiryDateCalculator cal = new ExpiryDateCalculator();
-        LocalDate expiryDate = cal.calculateExpiryDate(billingDate, payAmount);
-
-        assertEquals(LocalDate.of(2022,8,1), expiryDate);
-
-        //동일조건 예시 추가
-        LocalDate billingDate2 = LocalDate.of(2022,7,5);
-        int payAmount2 = 10_000;
-
-        ExpiryDateCalculator cal2 = new ExpiryDateCalculator();
-        LocalDate expiryDate2 = cal.calculateExpiryDate(billingDate2, payAmount);
-
-        assertEquals(LocalDate.of(2022,8,5), expiryDate2);
-
+        LocalDate realExpiryDate = cal.calculateExpiryDate(billingDate, payAmount);
+        assertEquals(expectedExpiryDate, realExpiryDate);
     }
 }
